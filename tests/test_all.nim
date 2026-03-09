c5ee6b2d2a32b7dd2393a87d7f298f1b59917c53
max <maxtclinton@gmail.com>
2026-03-09 17:59:31 -0400

    include all test modules in test runner

--- tests/test_all.nim
+++ tests/test_all.nim
@@ -5,9 +5,12 @@ import unittest2
 
 import ./test_serialize
 import ./test_crypto
+import ./test_address
 import ./test_script
 import ./test_consensus
 import ./test_storage
+import ./test_messages
+import ./test_peer
 
 when isMainModule:
   echo "Running all tests..."
