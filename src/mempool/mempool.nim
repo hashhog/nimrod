a6a7102cd2f7e580a44e868d6b0d614b155e1bbc
Max <maxtclinton@gmail.com>
2026-03-15 10:29:08 -0400

    implement ancestor/descendant limits for mempool

--- src/mempool/mempool.nim
+++ src/mempool/mempool.nim
@@ -21,6 +21,8 @@ type
     height*: int32          ## Block height when added
     ancestorFee*: Satoshi   ## Total fee of this tx plus all unconfirmed ancestors
     ancestorWeight*: int    ## Total weight of this tx plus all unconfirmed ancestors
+    ancestorCount*: int     ## Count of ancestors including self (cached for O(1) checks)
+    ancestorSize*: int      ## Total vsize of ancestors including self in vbytes (cached)
 
   Mempool* = ref object
     entries*: Table[TxId, MempoolEntry]
@@ -30,11 +32,24 @@ type
     minFeeRate*: float64    ## Minimum fee rate to accept (sat/vbyte)
     chainState*: ChainState
     params*: ConsensusParams
+    ## Package limits
+    ancestorLimit*: int     ## Max ancestor count including self (default 25)
+    descendantLimit*: int   ## Max descendant count including self (default 25)
+    ancestorSizeLimit*: int ## Max total ancestor vsize in vbytes (default 101,000)
+    descendantSizeLimit*: int ## Max total descendant vsize in vbytes (default 101,000)
 
 const
   DefaultMaxMempoolSize* = 300_000_000  ## 300 MB
   DefaultMinFeeRate* = 1.0              ## 1 sat/vbyte minimum
   MaxStandardTxWeight* = 400_000        ## 400K weight units max per tx
+
+  ## Package limits (Bitcoin Core defaults)
+  DefaultAncestorLimit* = 25            ## Max ancestors including self
+  DefaultDescendantLimit* = 25          ## Max descendants including self
+  DefaultAncestorSizeLimitKvB* = 101    ## Max total ancestor vsize in kvB
+  DefaultDescendantSizeLimitKvB* = 101  ## Max total descendant vsize in kvB
+  DefaultAncestorSizeLimit* = DefaultAncestorSizeLimitKvB * 1000  ## 101,000 vbytes
+  DefaultDescendantSizeLimit* = DefaultDescendantSizeLimitKvB * 1000  ## 101,000 vbytes
 
 # Result type for mempool operations
 type
@@ -57,7 +72,11 @@ proc evictLowestFee*(mp: var Mempool)
 # Constructor
 proc newMempool*(chainState: ChainState, params: ConsensusParams,
                  maxSize: int = DefaultMaxMempoolSize,
-                 minFeeRate: float64 = DefaultMinFeeRate): Mempool =
+                 minFeeRate: float64 = DefaultMinFeeRate,
+                 ancestorLimit: int = DefaultAncestorLimit,
+                 descendantLimit: int = DefaultDescendantLimit,
+                 ancestorSizeLimit: int = DefaultAncestorSizeLimit,
+                 descendantSizeLimit: int = DefaultDescendantSizeLimit): Mempool =
   Mempool(
     entries: initTable[TxId, MempoolEntry](),
     spentBy: initTable[OutPoint, TxId](),
@@ -65,7 +84,11 @@ proc newMempool*(chainState: ChainState, params: ConsensusParams,
     currentSize: 0,
     minFeeRate: minFeeRate,
     chainState: chainState,
-    params: params
+    params: params,
+    ancestorLimit: ancestorLimit,
+    descendantLimit: descendantLimit,
+    ancestorSizeLimit: ancestorSizeLimit,
+    descendantSizeLimit: descendantSizeLimit
   )
 
 # Basic accessors
@@ -168,6 +191,133 @@ proc calculateAncestorFeesAndWeight*(mp: Mempool, tx: Transaction,
 
   (Satoshi(totalFee), totalWeight)
 
+# Calculate ancestor set for a transaction (BFS through parents)
+proc calculateAncestors*(mp: Mempool, tx: Transaction): HashSet[TxId] =
+  ## Return the set of all ancestor txids in the mempool (not including self)
+  var visited = initHashSet[TxId]()
+  var toVisit: seq[TxId]
+
+  # Collect immediate parents
+  for input in tx.inputs:
+    if input.prevOut.txid in mp.entries:
+      toVisit.add(input.prevOut.txid)
+
+  # BFS through ancestors
+  while toVisit.len > 0:
+    let parentTxid = toVisit.pop()
+    if parentTxid in visited:
+      continue
+    visited.incl(parentTxid)
+
+    let parentEntry = mp.entries[parentTxid]
+    # Add grandparents
+    for input in parentEntry.tx.inputs:
+      if input.prevOut.txid in mp.entries and input.prevOut.txid notin visited:
+        toVisit.add(input.prevOut.txid)
+
+  visited
+
+# Calculate ancestor count and total vsize for a new transaction
+proc calculateAncestorStats*(mp: Mempool, tx: Transaction, selfVsize: int): (int, int) =
+  ## Returns (ancestor count including self, total ancestor vsize including self)
+  let ancestors = mp.calculateAncestors(tx)
+  var totalVsize = selfVsize
+  for ancestorTxid in ancestors:
+    let entry = mp.entries[ancestorTxid]
+    # vsize = weight / 4 (rounded up)
+    totalVsize += (entry.weight + 3) div 4
+
+  (len(ancestors) + 1, totalVsize)  # +1 for self
+
+# Calculate descendant set for a transaction (BFS through children)
+proc calculateDescendants*(mp: Mempool, txid: TxId): HashSet[TxId] =
+  ## Return the set of all descendant txids in the mempool (not including self)
+  var visited = initHashSet[TxId]()
+  var toVisit: seq[TxId]
+
+  # Find immediate children (transactions that spend outputs of this tx)
+  for otherTxid, entry in mp.entries:
+    for input in entry.tx.inputs:
+      if input.prevOut.txid == txid:
+        toVisit.add(otherTxid)
+        break
+
+  # BFS through descendants
+  while toVisit.len > 0:
+    let childTxid = toVisit.pop()
+    if childTxid in visited:
+      continue
+    visited.incl(childTxid)
+
+    # Add grandchildren
+    for otherTxid, entry in mp.entries:
+      for input in entry.tx.inputs:
+        if input.prevOut.txid == childTxid and otherTxid notin visited:
+          toVisit.add(otherTxid)
+          break
+
+  visited
+
+# Calculate descendant count and total vsize for an entry
+proc calculateDescendantStats*(mp: Mempool, txid: TxId): (int, int) =
+  ## Returns (descendant count including self, total descendant vsize including self)
+  let entry = mp.entries[txid]
+  let descendants = mp.calculateDescendants(txid)
+  var totalVsize = (entry.weight + 3) div 4  # Self vsize
+
+  for descTxid in descendants:
+    let descEntry = mp.entries[descTxid]
+    totalVsize += (descEntry.weight + 3) div 4
+
+  (len(descendants) + 1, totalVsize)  # +1 for self
+
+# Check package limits for a new transaction
+proc checkPackageLimits*(mp: Mempool, tx: Transaction, weight: int): MempoolResult[void] =
+  ## Check if adding this transaction would violate ancestor/descendant limits
+  ## Returns ok(()) if limits are satisfied, err(msg) otherwise
+
+  let vsize = (weight + 3) div 4
+
+  # Calculate ancestor stats for the new transaction
+  let (ancestorCount, ancestorSize) = mp.calculateAncestorStats(tx, vsize)
+
+  # Check ancestor count limit (including self)
+  if ancestorCount > mp.ancestorLimit:
+    return err(void, "exceeds ancestor limit: " & $ancestorCount & " > " & $mp.ancestorLimit)
+
+  # Check ancestor size limit
+  if ancestorSize > mp.ancestorSizeLimit:
+    return err(void, "exceeds ancestor size limit: " & $ancestorSize & " vB > " & $mp.ancestorSizeLimit & " vB")
+
+  # Check descendant limits for each ancestor
+  # Adding this tx increases each ancestor's descendant count by 1 and size by vsize
+  let ancestors = mp.calculateAncestors(tx)
+  for ancestorTxid in ancestors:
+    let (descCount, descSize) = mp.calculateDescendantStats(ancestorTxid)
+    # After adding the new tx, the ancestor would have descCount + 1 descendants
+    # and descSize + vsize total descendant size
+    if descCount + 1 > mp.descendantLimit:
+      return err(void, "would exceed descendant limit for ancestor " & $ancestorTxid &
+                       ": " & $(descCount + 1) & " > " & $mp.descendantLimit)
+    if descSize + vsize > mp.descendantSizeLimit:
+      return err(void, "would exceed descendant size limit for ancestor " & $ancestorTxid &
+                       ": " & $(descSize + vsize) & " vB > " & $mp.descendantSizeLimit & " vB")
+
+  # Also check immediate parents (mempool entries this tx spends from)
+  # These need the descendant check too
+  for input in tx.inputs:
+    if input.prevOut.txid in mp.entries:
+      let parentTxid = input.prevOut.txid
+      if parentTxid notin ancestors:
+        # This shouldn't happen, but handle it anyway
+        let (descCount, descSize) = mp.calculateDescendantStats(parentTxid)
+        if descCount + 1 > mp.descendantLimit:
+          return err(void, "would exceed descendant limit for parent " & $parentTxid)
+        if descSize + vsize > mp.descendantSizeLimit:
+          return err(void, "would exceed descendant size limit for parent " & $parentTxid)
+
+  MempoolResult[void](isOk: true)
+
 # Accept a transaction into the mempool
 proc acceptTransaction*(mp: var Mempool, tx: Transaction,
                         crypto: CryptoEngine): MempoolResult[TxId] =
@@ -264,6 +414,15 @@ proc acceptTransaction*(mp: var Mempool, tx: Transaction,
   # Calculate ancestor fees and weight for CPFP
   let (ancestorFee, ancestorWeight) = mp.calculateAncestorFeesAndWeight(tx, fee, weight)
 
+  # Check package limits (ancestor/descendant count and size)
+  let packageLimitsResult = mp.checkPackageLimits(tx, weight)
+  if not packageLimitsResult.isOk:
+    return err(TxId, "package limits exceeded: " & packageLimitsResult.error)
+
+  # Calculate ancestor stats for caching
+  let vsizeInt = (weight + 3) div 4
+  let (ancestorCount, ancestorSize) = mp.calculateAncestorStats(tx, vsizeInt)
+
   # Check mempool size limit - evict if needed
   let txSize = serialize(tx).len
   while mp.currentSize + txSize > mp.maxSize:
@@ -281,7 +440,9 @@ proc acceptTransaction*(mp: var Mempool, tx: Transaction,
     timeAdded: getTime(),
     height: mp.chainState.bestHeight,
     ancestorFee: ancestorFee,
-    ancestorWeight: ancestorWeight
+    ancestorWeight: ancestorWeight,
+    ancestorCount: ancestorCount,
+    ancestorSize: ancestorSize
   )
 
   # Add to mempool
